a82a7c5c93a9608f45c01b6c8bc51168c1e184bf
yuandonghu <yuandong.hu@we-wash.com>
2020-01-07 13:15:20 +0100

    add cross origin in controller

--- src/main/java/com/tree/controller/TreeController.java
+++ src/main/java/com/tree/controller/TreeController.java
@@ -4,11 +4,13 @@ import com.tree.model.Node;
 import com.tree.service.TreeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(maxAge = 3600)
 @RequestMapping("/v1")
 public class TreeController {
     private final TreeService treeService;
